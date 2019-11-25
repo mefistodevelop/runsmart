05ca93704dafe3dd7ffdf0f31d804fe81761d690
Oleg Rudenko <oleg.ru93@yandex.ru>
2019-11-25 20:10:44 +0300

    Добавлена маска ввода номера телефона с помощью плагина Masked Input

--- src/js/script.js
+++ src/js/script.js
@@ -75,4 +75,6 @@ $(document).ready(function(){
   validateForm('#consultation form');
   validateForm('#consultation-form');
   validateForm('#order form');
+
+  $('input[name=phone]').mask('+7 (999) 999-99-99');
 });
