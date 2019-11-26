4e0e3ae8be67affa535b4a5a1d57e9f201015f0b
Oleg Rudenko <oleg.ru93@yandex.ru>
2019-11-26 20:51:33 +0300

    Добавлена возможность отправлять данные из формы на почту
    используется плагин PHP Mailer

--- src/js/script.js
+++ src/js/script.js
@@ -77,4 +77,20 @@ $(document).ready(function(){
   validateForm('#order form');
 
   $('input[name=phone]').mask('+7 (999) 999-99-99');
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: 'POST',
+      url: 'mailer/smart.php',
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find('input').val('');
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn();
+
+      $('form').trigger('reset');
+    });
+    return false;
+  });
 });
